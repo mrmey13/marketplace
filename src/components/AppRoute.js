7829c0dd93506c88032523a8f6ba42b9fb28c6c6
VUTHANHUET <71000712+VUTHANHUET@users.noreply.github.com>
2021-09-17 10:07:54 +0700

    update  giao diện shop

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,46 +1,48 @@
 import React, { Component } from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    Switch,
-    Redirect,
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
 } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 
 import cs from "../const";
+import ShopView from "../components/ShopView/ShopView";
 import { getRole, getFunctionRoles } from "../service";
 import { ToastProvider } from "react-toast-notifications";
 import { useTranslation, withTranslation } from "react-i18next";
 
 const styles = (theme) => ({
-    tableContainer: {
-        height: 320,
-    },
+  tableContainer: {
+    height: 320,
+  },
 });
 
 export class AppRoute extends Component {
-    constructor(props) {
-        super(props);
-        // console.log("AppRoute props", props);
-    }
-    render(props) {
-        const { t, i18n } = this.props;
-        return (
-            <ToastProvider autoDismissTimeout={5000} placement="bottom-center">
-                <Route exact path="/" component={() => DashboardArea(t, i18n)} />
-            </ToastProvider>
-        )
-    }
+  constructor(props) {
+    super(props);
+    // console.log("AppRoute props", props);
+  }
+  render(props) {
+    const { t, i18n } = this.props;
+    return (
+      <ToastProvider autoDismissTimeout={5000} placement="bottom-center">
+        <Route exact path="/" component={() => DashboardArea(t, i18n)} />
+        <Route exact path="/shop_view" component={() => ShopView(t, i18n)} />
+      </ToastProvider>
+    );
+  }
 }
 
 const DashboardArea = (t, i18n) => (
-    <div>
-      <Typography component="div" className={styles.tableContainer}>
-        {/* E-Training Home */}
-        {t("app_route.dashboard")}
-      </Typography>
-    </div>
-  );
+  <div>
+    <Typography component="div" className={styles.tableContainer}>
+      {/* E-Training Home */}
+      {t("app_route.dashboard")}
+    </Typography>
+  </div>
+);
 
 export default withTranslation()(AppRoute);

0d00d37920f2916d3d5c4397bb11368230487974
anh.tran <anhesrever@gmail.com>
2021-09-17 11:42:25 +0700

    Them chuc nang tao/sua thong tin co ban cua shop

--- src/components/ShopProfile/ShopProfile.js
+++ src/components/ShopProfile/ShopProfile.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import { useTranslation, withTranslation } from "react-i18next";
+import { withToastManager } from 'react-toast-notifications';
 
 import cs from "../../const";
 import default_image from '../../img/default_image_gallery.png';
 import { Typography } from 'antd';
 import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+const storeDetailsURL = cs.BaseURL + '/api/seller/shop/detail';
+const createStoreURL = cs.BaseURL + '/api/seller/shop/create';
+const editStoreURL = cs.BaseURL + '/api/seller/shop/edit';
+
 const styles = (theme) => ({
     bgImage: {
         height: "300px",
-        width: "100%"
+        width: "100%",
+        objectFit:"cover"
     },
     avatarImage: {
         position: "absolute",
@@ -29,9 +37,45 @@ class ShopProfile extends Component {
         super(props);
         this.state = {
             bg: null,
-            avatar: null
+            avatar: null,
+            name:"",
+            description:"",
+            created: false,
         }
     }
+
+    componentWillMount(){
+        this.loadData();
+    }
+
+    loadData = conditions => {
+
+        const queryString = storeDetailsURL;
+
+
+        fetch(queryString, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: localStorage.getItem(cs.System_Code + '-token')
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data && data.error_code == 0 && data.error_desc == "Success") {
+                    this.setState({
+                        created: true,
+                        shopName: data.data.shopName,
+                        description: data.data.description
+                    });
+                } else {
+                    console.log("HERE");
+                }
+            })
+            .catch(() => {
+            });
+    };
 
     handleBGChange = (event) => {
         const newImage = URL.createObjectURL(event.target?.files?.[0]);
@@ -53,24 +97,100 @@ class ShopProfile extends Component {
         }
     };
 
+    handleChange = name => event => {
+        if (name == 'bg' || name == 'avatar') {
+            const newImage = URL.createObjectURL(event.target?.files?.[0]);
+
+            if (newImage) {
+                console.log(newImage);
+                this.setState({ [name]: newImage })
+                // setImage(URL.createObjectURL(newImage));
+            }
+        }
+        else
+            this.setState(
+                {
+                    [name]: event.target.value
+                },
+                () => {
+                    
+                }
+            );
+    };
+
+    handleDialogAgree = () => {
+        //tmpData = data.data.filter((el) => el.is_deleted_by_merchant === 0);
+        if (!this.state.shopName ||
+            !this.state.description
+        ) {
+            window.alert('Please input information !');
+            return;
+        }
+        //let agent = this.state.agents.filter((el) => el.id === this.state.agent_id)
+
+        // if (this.state.amount && isNaN(this.state.amount)) {
+        //     window.alert('Incorrect amount !');
+        //     return;
+        // }
+
+        let queryString = `${this.state.created ? editStoreURL : createStoreURL}`;
+        let requestBody = {
+            shopName:this.state.shopName,
+            description: this.state.description
+        };
+
+        console.log(requestBody);
+        fetch(queryString, {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem(cs.System_Code + '-token')
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // if (data && (data.code == cs.erAuthenticationFailed.code || data.code == cs.erAccountIsLocked.code)) {
+                //     localStorage.clear();
+                //     sessionStorage.clear();
+                //     window.location.reload();
+                // }
+
+                if (data && data.error_code != 0 && data.error_desc != "Success") {
+                    this.props.toastManager.add(JSON.stringify(data.error), {
+                        appearance: 'error',
+                        autoDismiss: true,
+                        pauseOnHover: true
+                    });
+                } else {
+                    this.props.toastManager.add(this.state.created ? 'Shop is edited !' : 'Shop is created !', {
+                        appearance: 'success',
+                        autoDismiss: true,
+                        pauseOnHover: true
+                    });
+                    //                    window.location.href="/package-info;
+                }
+                // window.location.reload();
+
+            })
+            .catch(() => { });
+    };
+
     render() {
-        const { classes } = this.props;
+        const { classes, t, i18n } = this.props;
         return (
             <div className={"card"}>
                 <div className="card-body shadow">
                     <div className="d-flex align-items-baseline">
                         <h4 className="card-title mb-4 text-uppercase">
-                            Thông tin cơ bản
+                            {t("shop_profile.basic_info")}
                         </h4>
                     </div>
                     <div className="row">
                         <div className="col-sm-3 card card-body me-sm-1">
-                            {/* <div style={{backgroundImage:URL.createObjectURL("../../img/default_image_gallery.png") }}>
-                                trgr
-                            </div> */}
-                            {/* <image src={this.state.bg || require("../../img/default_image_gallery.png")}>
-
-                            </image> */}
+                            
                             <div >
                                 <img src={this.state.bg || default_image} className={classes.bgImage}>
 
@@ -79,10 +199,33 @@ class ShopProfile extends Component {
                             </div>
 
                             <div>
-                                <Typography>Background</Typography>
-                                <input type="file" onChange={this.handleBGChange} />
-                                <Typography>Avatar</Typography>
-                                <input type="file" onChange={this.handleAvatarChange} />
+                                <Typography>{t("shop_profile.background")}</Typography>
+                                {/* <input type="file" id="bg" onChange={this.handleChange('bg')} /> */}
+                                <Button
+                                variant="contained"
+                                component="label"
+                                >
+                                    Upload File
+                                    <input
+                                        type="file"
+                                        hidden
+                                        onChange={this.handleChange('bg')}
+                                    />
+                                </Button>
+
+                                <Typography>{t("shop_profile.avatar")}</Typography>
+                                {/* <input type="file" id="avatar" onChange={this.handleChange('avatar')} /> */}
+                                <Button
+                                variant="contained"
+                                component="label"
+                                >
+                                    Upload File
+                                    <input
+                                        type="file"
+                                        hidden
+                                        onChange={this.handleChange('avatar')}
+                                    />
+                                </Button>
                             </div>
 
                             {/* <img src={this.state.bg || default_image}/> */}
@@ -90,31 +233,38 @@ class ShopProfile extends Component {
                             {/* <img src={default_image}/> */}
                         </div>
                         <div className="col-sm-8 card card-body">
-                        <TextField
-                            disabled
-                            fullWidth
-                            autoFocus
-                            margin="dense"
-                            id="fullname"
-                            required={true}
-                            value={this.state.fullname}
-                            // onChange={this.handleChange('fullname')}
-                            label="Tên"
-                            type="name"
-                        />
 
-                        <TextField
-                            disabled
-                            fullWidth
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            required={true}
-                            value={this.state.name}
-                            // onChange={this.handleChange('name')}
-                            label="Tên"
-                            type="name"
-                        />
+                            <TextField
+                                // autofocus
+                                InputLabelProps={{ shrink: true }}
+                                fullWidth
+                                margin="dense"
+                                id="shopName"
+                                required={true}
+                                value={this.state.shopName}
+                                onChange={this.handleChange('shopName')}
+                                label={t("shop_profile.shopName")}
+                                type="name"
+                            />
+
+                            <TextField
+                            shrink
+                                // autofocus
+                                InputLabelProps={{ shrink: true }}
+                                multiline
+                                fullWidth
+                                margin="dense"
+                                id="description"
+                                required={true}
+                                value={this.state.description}
+                                onChange={this.handleChange('description')}
+                                label={t("shop_profile.description")}
+                                type="name"
+                            />
+
+                            <Button onClick={this.handleDialogAgree} variant="contained" color="primary">
+                                {this.state.created ? t("commons.button.edit") : t("commons.button.create")}
+                            </Button>
                         </div>
                     </div>
                 </div>
@@ -127,4 +277,4 @@ ShopProfile.propTypes = {
     classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(withTranslation()(ShopProfile));
+export default withToastManager(withStyles(styles)(withTranslation()(ShopProfile)));

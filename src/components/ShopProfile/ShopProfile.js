3fcd0555be0bbc924149b44b8fdab1d6599d6cc5
anh.tran <anhesrever@gmail.com>
2021-09-18 11:26:50 +0700

    Sua giao dien ShopProfile

--- src/components/ShopProfile/ShopProfile.js
+++ src/components/ShopProfile/ShopProfile.js
@@ -16,6 +16,9 @@ import ImageListItem from '@material-ui/core/ImageListItem';
 import { DropzoneDialog } from 'material-ui-dropzone';
 import UploadMedia from './UploadMedia';
 
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 const storeDetailsURL = cs.BaseURL + '/api/seller/shop/detail';
 const createStoreURL = cs.BaseURL + '/api/seller/shop/create';
 const editStoreURL = cs.BaseURL + '/api/seller/shop/edit';
@@ -34,7 +37,7 @@ function getYoutubeId(url) {
 }
 
 function getEmbededURL(url) {
-    
+
 }
 
 const styles = (theme) => ({
@@ -84,6 +87,22 @@ const styles = (theme) => ({
         padding: "8px 20px",
         fontSize: "12px",
         borderTop: "1px solid #e5e5e5"
+    },
+    delete_btn :{
+        width: "50px",
+        height: "50px",
+        // borderRadius: "25px",
+        position: "absolute",
+        backgroundColor: color.casablanca,
+        left: "-5px",
+        top: "0px",
+        '&:hover': {
+            backgroundColor: color.seabuckthorn,
+        },
+        '&:active': {
+            backgroundColor: color.seabuckthorn,
+        },
+        color:"#ffffff"
     }
 });
 
@@ -92,8 +111,14 @@ class ShopProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            bg: null,
+            bgURL: null,
+            bg:null,
+            bgUpdated: false,
+
+            avatarURL: null,
             avatar: null,
+            avatarUpdated: false,
+            
             name: "",
             description: "",
             mediaList: [],
@@ -136,34 +161,36 @@ class ShopProfile extends Component {
             });
     };
 
-    handleBGChange = (event) => {
-        const newImage = URL.createObjectURL(event.target?.files?.[0]);
-
-        if (newImage) {
-            console.log(newImage);
-            this.setState({ bg: newImage })
-            // setImage(URL.createObjectURL(newImage));
-        }
+    handleBGChange = () => {
+        console.log(this.state.bg);
     };
 
-    handleAvatarChange = (event) => {
-        const newImage = URL.createObjectURL(event.target?.files?.[0]);
-
-        if (newImage) {
-            console.log(newImage);
-            this.setState({ avatar: newImage })
-            // setImage(URL.createObjectURL(newImage));
-        }
+    handleAvatarChange = () => {
+        console.log(this.state.avatar);
     };
 
     handleChange = name => event => {
-        if (name == 'bg' || name == 'avatar') {
+        if (name == 'bg') {
             const newImage = URL.createObjectURL(event.target?.files?.[0]);
 
             if (newImage) {
                 console.log(newImage);
-                this.setState({ [name]: newImage })
-                // setImage(URL.createObjectURL(newImage));
+                this.setState({ 
+                    bgURL: newImage, 
+                    bg: event.target?.files?.[0],
+                    bgUpdated: true 
+                })
+            }
+        } else if (name == 'avatar') {
+            const newImage = URL.createObjectURL(event.target?.files?.[0]);
+
+            if (newImage) {
+                console.log(newImage);
+                this.setState({ 
+                    avatarURL: newImage, 
+                    avatar: event.target?.files?.[0],
+                    avatarUpdated: true 
+                })
             }
         }
         else
@@ -273,7 +300,7 @@ class ShopProfile extends Component {
     deleteMedia(id) {
         const { classes, t, i18n } = this.props;
 
-        var r = window.confirm(t("shop_profile.deleteMedia",{id:id}));
+        var r = window.confirm(t("shop_profile.deleteMedia", { id: id }));
         if (r == true) {
             const queryString = `${deleteMediaURL}?id=${id}`;
 
@@ -328,12 +355,12 @@ class ShopProfile extends Component {
                                 <div >
                                     {/* <div className={classes.cover}/> */}
                                     <div style={{ position: "relative" }}>
-                                        <img src={this.state.bg || default_image} className={classes.bgImage} />
+                                        <img src={this.state.bgURL || default_image} className={classes.bgImage} />
                                         <div className={classes.cover} />
                                     </div>
                                     {/* <img src={this.state.bg || default_image} className={classes.bgImage}> */}
 
-                                    <img src={this.state.avatar || default_image} className={classes.avatarImage} />
+                                    <img src={this.state.avatarURL || default_image} className={classes.avatarImage} />
                                     <Typography className={classes.previewText}>{t("shop_profile.shopName")}: {this.state.shopName}</Typography>
                                 </div>
 
@@ -341,7 +368,7 @@ class ShopProfile extends Component {
                                     <div className={classes.list_info}>
                                         <div className={classes.info_row}>
                                             <Typography>{t("shop_profile.avatar")}</Typography>
-                                            <Button
+                                            {!this.state.avatarUpdated && <Button
                                                 variant="contained"
                                                 component="label"
                                             >
@@ -351,11 +378,21 @@ class ShopProfile extends Component {
                                                     hidden
                                                     onChange={this.handleChange('avatar')}
                                                 />
-                                            </Button>
+                                            </Button>}
+                                            {this.state.avatarUpdated &&
+                                                <Button
+                                                    variant="contained"
+                                                    component="label"
+                                                    onClick={this.handleAvatarChange}
+                                                >
+                                                    {t("commons.button.save")}
+                                                </Button>
+                                            }
                                         </div>
+
                                         <div className={classes.info_row}>
                                             <Typography>{t("shop_profile.background")}</Typography>
-                                            <Button
+                                            {!this.state.bgUpdated && <Button
                                                 variant="contained"
                                                 component="label"
                                             >
@@ -365,7 +402,15 @@ class ShopProfile extends Component {
                                                     hidden
                                                     onChange={this.handleChange('bg')}
                                                 />
-                                            </Button>
+                                            </Button>}
+                                            {this.state.bgUpdated && <Button
+                                                variant="contained"
+                                                component="label"
+                                                onClick={this.handleBGChange}
+                                            >
+                                                {t("commons.button.save")}
+
+                                            </Button>}
                                         </div>
                                         <div className={classes.info_row}>
                                             <Typography>{t("shop_profile.shopView")}</Typography>
@@ -381,6 +426,7 @@ class ShopProfile extends Component {
 
                                 {/* <img src={default_image}/> */}
                             </div>
+
                             <div className="col-sm-8 card card-body">
 
                                 <TextField
@@ -431,35 +477,48 @@ class ShopProfile extends Component {
                         </div>
 
                         <div>
-                            <Button variant="contained" color="primary" onClick={() => this.setState({ openUploadModal: true })}>
+                            <Button 
+                                variant="contained" 
+                                color="primary" 
+                                onClick={() => this.setState({ openUploadModal: true })}
+                                style={{ backgroundColor: color.casablanca }}
+                            >
                                 Add Image
                             </Button>
 
-                            <div className="row" style={{paddingTop:"10px"}}>
+                            <div className="row" style={{ paddingTop: "30px" }}>
                                 {this.state.mediaList.map((item) => (
-                                    
-                                    <div className="col" style={{display:"flex", flexDirection:"column" }}>
+
+                                    <div className="col" style={{ display: "flex", flexDirection: "column", paddingTop:"20px",paddingBottom:"10px", position:"relative" }}>
+                                        <IconButton 
+                                        color="primary" 
+                                        onClick={() => this.deleteMedia(item.id)} 
+
+                                        className={classes.delete_btn} >
+                                            <DeleteIcon />
+                                        </IconButton>
+
                                         {item.type === 11 &&
                                             <img
-                                            id={item.id}
-                                            style={{  height: "400px", width:"800px" }}
-                                            src={`${cs.MediaURL}/media/${item.path}`}
-                                            srcSet={`${cs.MediaURL}/media/${item.path}`}
-                                            alt={item.title}
-                                            loading="lazy"
+                                                id={item.id}
+                                                style={{ height: "400px", width: "800px", borderStyle: "solid", }}
+                                                src={`${cs.MediaURL}/media/${item.path}`}
+                                                srcSet={`${cs.MediaURL}/media/${item.path}`}
+                                                alt={item.title}
+                                                loading="lazy"
                                             />
                                         }
                                         {item.type === 22 &&
-                                            <iframe 
-                                            style={{  height: "400px", width:"800px" }}
-                                            src={`//www.youtube.com/embed/${getYoutubeId(item.path)}`} 
-                                            frameborder="0" allowfullscreen>
+                                            <iframe
+                                                style={{ height: "400px", width: "800px", borderStyle: "solid", }}
+                                                src={`//www.youtube.com/embed/${getYoutubeId(item.path)}`}
+                                                frameborder="0" allowfullscreen>
 
                                             </iframe>
                                         }
-                                        <Button onClick={() => this.deleteMedia(item.id)} style={{width:"800px"}}>
+                                        {/* <Button onClick={() => this.deleteMedia(item.id)} style={{ width: "800px",borderStyle: "solid", }}>
                                             {t("commons.button.delete")}
-                                        </Button>
+                                        </Button> */}
                                     </div>
                                 ))}
                             </div>

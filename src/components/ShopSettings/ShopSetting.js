ec1d5fa3c3f1094122bce9fc1a529e4e86bd2146
vantan.bui <tanbv@credito.asia>
2021-09-23 17:40:59 +0700

    setting trặng thái hoạt động của shop Signed-off-by: vantan.bui <tanbv@credito.asia>

--- src/components/ShopSettings/ShopSetting.js
+++ src/components/ShopSettings/ShopSetting.js
@@ -6,21 +6,21 @@ import color from "../../theme/color";
 import { useState, useEffect } from "react";
 import { useTranslation, withTranslation } from "react-i18next";
 
-
-const laguageURL = cs.BaseURL + '/api/common/language_definition';
-const settingURL = cs.BaseURL + '/api/seller/shop-setting';
+const laguageURL = cs.BaseURL + "/api/common/language_definition";
+const settingURL = cs.BaseURL + "/api/seller/shop-setting";
 
 const lngs = {
   en: { nativeName: "language.english" },
   vi: { nativeName: "language.vietnamese" },
 };
 
+let i = 0;
+
 class ShopSetting extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
 
   // const [language, setLanguage] = useState([]);
   // const loadLanguage = async () => {
@@ -55,6 +55,18 @@ class ShopSetting extends React.Component {
   //     console.log(error);
   //   }
   // }
+
+  changeClick = () => {
+    if (i % 2 === 0) {
+      alert(
+        "Người mua sẽ không thể đặt hàng trong khi Shop bạn tạm nghỉ bán. Bạn chắc chắn muốn bật tạm nghỉ bán chứ?"
+      );
+      i++;
+    } else {
+      alert("Bạn chắc chắn muốn tắt tạm nghỉ bán chứ?");
+      i++;
+    }
+  };
 
   render() {
     const { classes, t, i18n } = this.props;
@@ -231,7 +243,7 @@ class ShopSetting extends React.Component {
                               type="checkbox"
                               id="flexSwitchCheckDefault"
                               style={{ width: "50px", height: "25px" }}
-                              // onClick={() => }
+                              onClick={() => this.changeClick()}
                             />
                             <label
                               class="form-check-label"
@@ -327,7 +339,8 @@ class ShopSetting extends React.Component {
                                 backgroundColor: "#F69756",
                                 width: "82px",
                                 height: "30px",
-                                borderRadius: "10px",
+                                border: "1px solid",
+                                borderRadius: "15px",
                                 marginLeft: "10px",
                               }}
                               type="submit"
